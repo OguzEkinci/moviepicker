8b6de72e2ad3e428eea15743898f4e16ce082d0f
OguzEkinci <by.engineer16@gmail.com>
2022-12-28 00:46:30 +0300

    Update DiscoverCard.style.js

--- src/components/DiscoverCard/DiscoverCard.style.js
+++ src/components/DiscoverCard/DiscoverCard.style.js
@@ -3,12 +3,8 @@ const { width, height } = Dimensions.get('screen')
 const styles = StyleSheet.create({
   discoverCard: {
     margin: 4,
-    width: width / 3,
-    height: 250,
-    backgroundColor: 'transparent',
-    borderRadius: 10,
-    borderColor: '#484848',
-    borderWidth: 2,
+    width: width / 1.5,
+    height: 200,
     justifyContent: 'center',
     alignItems: 'center'
   }
