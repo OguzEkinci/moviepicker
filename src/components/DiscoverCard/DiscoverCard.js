62bdd0cb391e4f49eb02c5f9653801efcb632c0e
OguzEkinci <by.engineer16@gmail.com>
2022-12-28 00:46:27 +0300

    Update DiscoverCard.js

--- src/components/DiscoverCard/DiscoverCard.js
+++ src/components/DiscoverCard/DiscoverCard.js
@@ -1,13 +1,20 @@
+import { BlurView } from '@react-native-community/blur'
 import React, { useState } from 'react'
 import {
   SafeAreaView,
   View,
   Text,
   TouchableWithoutFeedback,
-  TouchableOpacity
+  TouchableOpacity,
+  Dimensions
 } from 'react-native'
+import ImagedCarouselCard from 'react-native-imaged-carousel-card'
+import { MovieInfoModal } from '../MovieModal/MovieModal'
 import { styles } from './DiscoverCard.style'
-const DiscoverCard = ({ item, setModalVisible }) => {
+const { width, height } = Dimensions.get('window')
+const DiscoverCard = ({ item }) => {
+  const [modalVisible, setModalVisible] = useState(false)
+
   return (
     <View style={styles.discoverCard}>
       <TouchableWithoutFeedback
@@ -18,11 +25,23 @@ const DiscoverCard = ({ item, setModalVisible }) => {
         <View
           style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
         >
-          <Text>
-            {item.title}
-          </Text>
+          <ImagedCarouselCard
+            text={item?.title}
+            textStyle={{color: '#ddd', fontSize: 16, fontWeight: 'bold', textAlign: 'center'}}
+            style={styles.discoverCard}
+            width={width / 1.5}
+            height={100}
+            shadowColor='#051934'
+            source={require('../../assets/background.png')}
+            // source={{ uri: `https://image.tmdb.org/t/p/w500/${item?.poster_path}`}}
+          />
         </View>
       </TouchableWithoutFeedback>
+      <MovieInfoModal
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          movieInfo={item}
+        />
     </View>
   )
 }
