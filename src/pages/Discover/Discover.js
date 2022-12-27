0f294579b9f03ae17f3717e22dea4b3229a87d73
OguzEkinci <by.engineer16@gmail.com>
2022-12-28 00:46:42 +0300

    Update Discover.js

--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -13,18 +13,20 @@ import {
   FlatList
 } from 'react-native'
 import LottieView from 'lottie-react-native'
+import ImagedCarouselCard from 'react-native-imaged-carousel-card'
 import { FlashList } from '@shopify/flash-list'
 import { styles } from './Discover.style'
 import LinearGradient from 'react-native-linear-gradient'
 import { DiscoverCard } from '../../components/DiscoverCard/DiscoverCard'
-import { MovieInfoModal } from '../../components'
+import { Loader, MovieInfoModal } from '../../components'
 import { Portal, PortalHost } from '@gorhom/portal'
 import { getPopular } from '../../services/DiscoverServices/getPopular'
 const { width, height } = Dimensions.get('window')
 
 const Discover = ({ navigation }) => {
-  const [modalVisible, setModalVisible] = useState(false)
   const [popularList, setPopularList] = useState([])
+  const [loadingVisible, setLoadingVisible] = useState(false)
+
   const DATA = [
     {
       title: 'First Item'
@@ -44,13 +46,19 @@ const Discover = ({ navigation }) => {
   ]
 
   useEffect(() => {
+    setLoadingVisible(true)
     getPopular()
-      .then(res => setPopularList(res.data.results))
-      .catch(err => console.log('popular err', err))
+      .then(res => {
+        setLoadingVisible(false)
+        setPopularList(res.data.results)
+      })
+      .catch(err => {
+        setLoadingVisible(false)
+      })
   }, [])
 
   const renderGetPopular = ({ item }) => {
-    return <DiscoverCard item={item} setModalVisible={setModalVisible} />
+    return <DiscoverCard item={item} />
   }
   return (
     <SafeAreaView style={styles.container}>
@@ -72,12 +80,7 @@ const Discover = ({ navigation }) => {
             estimatedItemSize={400}
           />
         </ScrollView>
-        <MovieInfoModal
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-          /* movieInfo={randomMovie}
-        setMovieInfo={setRandomMovie} */
-        />
+        {loadingVisible && <Loader />}
       </LinearGradient>
     </SafeAreaView>
   )
