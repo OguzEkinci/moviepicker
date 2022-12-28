99335ca8e08731d0382564f7ef6d0c8df4035c11
OguzEkinci <by.engineer16@gmail.com>
2022-12-28 03:12:36 +0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { View, Text, Dimensions } from 'react-native'
-import Modal from "react-native-modal";
+import {View, Text, Dimensions} from 'react-native';
+import Modal from 'react-native-modal';
 import LottieView from 'lottie-react-native';
 const Loader = () => {
-    return (
-        <Modal  isVisible={true} transparent={true}>
-            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-                <LottieView
-                    style={{ height: 400 }}
-                    source={require('../../animations/1961-movie-loading.json')}
-                    autoPlay
-                    loop
-                />
-            </View>
-        </Modal>)
-}
-export { Loader }
+  return (
+    <View>
+      <Modal isVisible={true} transparent={true}>
+        <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+          <LottieView
+            style={{height: 400}}
+            source={require('../../animations/1961-movie-loading.json')}
+            autoPlay
+            loop
+          />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+export {Loader};
