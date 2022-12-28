04d00e7920299b72f7022954b6045478c858c27d
OguzEkinci <by.engineer16@gmail.com>
2022-12-28 03:12:31 +0300

    Update DiscoverCard.style.js

--- src/components/DiscoverCard/DiscoverCard.style.js
+++ src/components/DiscoverCard/DiscoverCard.style.js
@@ -1,12 +1,12 @@
-import { Dimensions, StyleSheet } from 'react-native'
-const { width, height } = Dimensions.get('screen')
+import {Dimensions, StyleSheet} from 'react-native';
+const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
   discoverCard: {
     margin: 4,
-    width: width / 1.5,
-    height: 200,
+    width: width / 3,
+    height: height / 4.5,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
-export { styles }
+    alignItems: 'center',
+  },
+});
+export {styles};
