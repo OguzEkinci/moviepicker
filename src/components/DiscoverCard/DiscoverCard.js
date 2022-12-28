7fbc483e10b0077930d472fea31b2640d962fa65
OguzEkinci <by.engineer16@gmail.com>
2022-12-28 03:12:27 +0300

    Update DiscoverCard.js

--- src/components/DiscoverCard/DiscoverCard.js
+++ src/components/DiscoverCard/DiscoverCard.js
@@ -1,48 +1,57 @@
-import { BlurView } from '@react-native-community/blur'
-import React, { useState } from 'react'
+import {BlurView} from '@react-native-community/blur';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   View,
   Text,
   TouchableWithoutFeedback,
   TouchableOpacity,
-  Dimensions
-} from 'react-native'
-import ImagedCarouselCard from 'react-native-imaged-carousel-card'
-import { MovieInfoModal } from '../MovieModal/MovieModal'
-import { styles } from './DiscoverCard.style'
-const { width, height } = Dimensions.get('window')
-const DiscoverCard = ({ item }) => {
-  const [modalVisible, setModalVisible] = useState(false)
+  Dimensions,
+} from 'react-native';
+import ImagedCarouselCard from 'react-native-imaged-carousel-card';
+import {MovieInfoModal} from '../MovieModal/MovieModal';
+import {styles} from './DiscoverCard.style';
+const {width} = Dimensions.get('window');
+const DiscoverCard = ({item}) => {
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <View style={styles.discoverCard}>
       <TouchableWithoutFeedback
         onPress={() => {
-          setModalVisible(true)
-        }}
-      >
+          setModalVisible(true);
+        }}>
         <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
           <ImagedCarouselCard
             text={item?.title}
-            textStyle={{color: '#ddd', fontSize: 16, fontWeight: 'bold', textAlign: 'center'}}
+            textStyle={{
+              color: '#ddd',
+              fontSize: 16,
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}
+            overlayBackgroundColor="#051934"
             style={styles.discoverCard}
-            width={width / 1.5}
+            width={width / 3}
             height={100}
-            shadowColor='#051934'
-            source={require('../../assets/background.png')}
-            // source={{ uri: `https://image.tmdb.org/t/p/w500/${item?.poster_path}`}}
+            shadowColor="#051934"
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500/${item?.poster_path}`,
+            }}
           />
         </View>
       </TouchableWithoutFeedback>
       <MovieInfoModal
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-          movieInfo={item}
-        />
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        movieInfo={item}
+      />
     </View>
-  )
-}
-export { DiscoverCard }
+  );
+};
+export {DiscoverCard};
