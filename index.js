c3a4bd14b135623b87e03b86e6b0a7c0f42fab51
OguzEkinci <by.engineer16@gmail.com>
2022-12-30 04:09:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './src/Route.js';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import {store} from './src/redux/store';
+import {Provider} from 'react-redux';
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+AppRegistry.registerComponent(appName, () => Root);
