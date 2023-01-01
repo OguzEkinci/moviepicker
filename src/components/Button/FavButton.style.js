1e399cd36884e11cc2332b523378b1cf610ebb02
OguzEkinci <by.engineer16@gmail.com>
2023-01-01 05:31:28 +0300

    Update FavButton.style.js

--- src/components/Button/FavButton.style.js
+++ src/components/Button/FavButton.style.js
@@ -2,9 +2,8 @@ import {Dimensions, StyleSheet} from 'react-native';
 const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
   image: {
-    width: 35,
-    height: 35,
-    tintColor: '#ff685d',
+    width: 65,
+    height: 70,
   },
 });
 export {styles};
