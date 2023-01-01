c615b70871bf10d5850c6a827a5727ba1a329a71
OguzEkinci <by.engineer16@gmail.com>
2023-01-01 05:31:50 +0300

    Update MovieModal.style.js

--- src/components/MovieModal/MovieModal.style.js
+++ src/components/MovieModal/MovieModal.style.js
@@ -1,65 +1,87 @@
-import { Dimensions, StyleSheet } from 'react-native'
-const { width, height } = Dimensions.get('screen')
+import {Dimensions, StyleSheet} from 'react-native';
+const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink',
+    borderRadius: 10,
+    flex: 0.9,
+    paddingTop: 0,
+    paddingBottom: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
   mainTitle: {
     width: 340,
     textAlign: 'center',
     color: '#191919',
     fontSize: 40,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   subTitle: {
     width: 340,
     textAlign: 'center',
     color: '#484848',
     fontSize: 17,
-    marginTop: 8
+    marginTop: 8,
   },
   yearAndTimeHeader: {
     width: 340,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-evenly'
+    justifyContent: 'space-evenly',
   },
   yearAndTimeHeaderInside: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 8,
-    marginBottom: 8
+    marginBottom: 8,
   },
   mainText: {
     textAlign: 'center',
     color: '#484848',
     fontSize: 15,
     width: 340,
-    padding: 4
+    padding: 4,
   },
   genresView: {
     width: 340,
     flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   genresInsideView: {
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#a0587e',
     margin: 8,
     borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#484848',
-    padding: 4
+    padding: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 8,
+  },
+  genresText: {
+    color: '#ddd',
+    padding: 2,
   },
   otherInfoView: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'center',
     width: 340,
-    marginTop: 16
+    marginTop: 16,
   },
   icon: {
     width: 15,
     height: 15,
-    marginRight: 3
-  }
-})
-export { styles }
+    marginRight: 3,
+  },
+});
+export {styles};
