f192d5f664307460ed9e198bf7a601b4cf2bf35a
OguzEkinci <by.engineer16@gmail.com>
2023-01-11 02:53:46 +0300

    Update GetVideo.js

--- src/services/GetVideo/GetVideo.js
+++ src/services/GetVideo/GetVideo.js
@@ -4,6 +4,6 @@ import {locale} from '../../assets/util/phoneLanguage';
 export const getVideo = id => {
   return axios({
     method: 'get',
-    url: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=ae3bb45bf136ab578d22629b6eec6017&language=${locale}`,
+    url: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=ae3bb45bf136ab578d22629b6eec6017`,
   });
 };
