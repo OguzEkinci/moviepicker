ffb2555161dc88c65d1f3facd864cb1c31adf702
OguzEkinci <by.engineer16@gmail.com>
2023-01-11 03:48:39 +0300

    Update Fav.style.js

--- src/pages/Fav/Fav.style.js
+++ src/pages/Fav/Fav.style.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     marginRight: 3,
     width: width,
     flexDirection: 'row',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
 });
