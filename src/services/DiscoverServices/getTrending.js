5479713eca304ed55f08098d94092762765a677c
OguzEkinci <by.engineer16@gmail.com>
2023-02-27 12:42:51 +0300

    Update getTrending.js

--- src/services/DiscoverServices/getTrending.js
+++ src/services/DiscoverServices/getTrending.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import {locale} from '../../assets/util/phoneLanguage';
 
 export const getTrending = timeRange => {
   return axios({
     method: 'get',
-    url: `https://api.themoviedb.org/3/trending/movie/${timeRange}?api_key=ae3bb45bf136ab578d22629b6eec6017`,
+    url: `https://api.themoviedb.org/3/trending/movie/${timeRange}?api_key=ae3bb45bf136ab578d22629b6eec6017&language=${locale}`,
   });
 };
